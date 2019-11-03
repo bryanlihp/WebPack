3c00b04a4145e3f3bc14166f7653ab8df2fcc22d
ColdBeer <bryanlihp@hotmail.com>
2019-11-03 15:04:07 -0800

    add es6 and react

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,7 +1,10 @@
-var config = require('./GreeterConfig.json'); //webpack3.* plus has internal JSON loader, no json-loader is required.
+import React from "react";
+import config from "./GreeterConfig.json";
 
-module.exports = function() {
-    var greet = document.createElement('div');
-    greet.textContent=config.greetText;
-    return greet;
+export default class Greater extends React.Component{
+    render (){
+        return (
+            <div>{config.greetText}</div>
+        );
+    }
 }
